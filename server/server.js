b3ebef05dc0aaf7793fae5d630b1d6ee6ec26841
zaid <zaidhusseini@gmail.com>
2018-03-21 19:00:28 -0700

    Update server side code to fetch from public file

--- server/server.js
+++ server/server.js
@@ -3,32 +3,33 @@ const path = require("path");
 var bodyParser = require("body-parser");
 // var router = require("./routes/routes.js");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
+const host = process.env.HOST || 'localhost';
 
 
-app.use('/restaurants/:id', express.static('public'));
+app.use('/restaurants/:id', express.static('../public'));
 
 app.get('/', (req, res) => {
-  res.sendFile('../../public/index.html');
+  res.redirect(`http://${host}:${port}/restaurants/${Math.floor(Math.random()*10000000)}`);
 });
 
 app.get('/api/restaurants/:id/gallery', (req, res) => {
-  res.redirect(`http://localhost:3001/api/restaurants/${req.params.id}/gallery`)
+  res.redirect(`http://${host}:3002/api/restaurants/${req.params.id}/gallery`)
 });
 
 app.get('/api/restaurants/:id/overview', (req, res) => {
-  res.redirect(`http://localhost:3002/api/restaurants/${req.params.id}/overview`)
+  res.redirect(`http://${host}:3002/api/restaurants/${req.params.id}/overview`)
 });
 
 app.get('/api/restaurants/:id/sidebar', (req, res) => {
-  res.redirect(`http://localhost:3003/api/restaurants/${req.params.id}/sidebar`)
+  res.redirect(`http://${host}:3003/api/restaurants/${req.params.id}/sidebar`)
 });
 
 app.get('/api/restaurants/:id/recommendations', (req, res) => {
-  res.redirect(`http://127.0.0.1:3004/api/restaurants/${req.params.id}/recommendations`)
+  res.redirect(`http://${host}:3004/api/restaurants/${req.params.id}/recommendations`)
 });
 
 
 app.listen(port, () => {
-  console.log(`server running at: http://localhost:${port}`);
+  console.log(`server running at: http://${host}:${port}`);
 });
