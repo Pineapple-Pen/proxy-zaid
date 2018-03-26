c60d6885469901aa430394c438640480397f97f0
zaid <zaidhusseini@gmail.com>
2018-03-26 11:49:12 -0700

    Use env variables to define hosts

--- server/server.js
+++ server/server.js
@@ -1,40 +1,44 @@
 require('newrelic');
 const express = require("express");
 const path = require("path");
-var bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
+require("dotenv").load();
 
 //const morgan = require('morgan');
 
 // var router = require("./routes/routes.js");
 const app = express();
 const port = process.env.PORT || 3000;
-const host = process.env.HOST || 'localhost';
+const hostPhotos = process.env.HOST_PHOTOS || 'localhost';
+const hostOverview = process.env.HOST_OVERVIEW || 'localhost';
+const hostSidebar = process.env.HOST_SIDEBAR || 'localhost';
+const hostRecommendations = process.env.HOST_RECOMMENDATIONS || 'localhost';
 
 //app.use(morgan('tiny'));
 
 app.use('/restaurants/:id', express.static('public'));
 
 app.get('/', (req, res) => {
-  res.redirect(`http://${host}:${port}/restaurants/${Math.floor(Math.random()*10000000)}`);
+  res.redirect(`http://${hostPhotos}:${port}/restaurants/${Math.floor(Math.random()*10000000)}`);
 });
 
 app.get('/api/restaurants/:id/gallery', (req, res) => {
-  res.redirect(`http://${host}:3002/api/restaurants/${req.params.id}/gallery`)
+  res.redirect(`http://${hostPhotos}:3002/api/restaurants/${req.params.id}/gallery`)
 });
 
 app.get('/api/restaurants/:id/overview', (req, res) => {
-  res.redirect(`http://${host}:3002/api/restaurants/${req.params.id}/overview`)
+  res.redirect(`http://${hostOverview}:3001/api/restaurants/${req.params.id}/overview`)
 });
 
 app.get('/api/restaurants/:id/sidebar', (req, res) => {
-  res.redirect(`http://${host}:3003/api/restaurants/${req.params.id}/sidebar`)
+  res.redirect(`http://${hostSidebar}:3003/api/restaurants/${req.params.id}/sidebar`)
 });
 
 app.get('/api/restaurants/:id/recommendations', (req, res) => {
-  res.redirect(`http://${host}:3004/api/restaurants/${req.params.id}/recommendations`)
+  res.redirect(`http://${hostRecommendations}:3004/api/restaurants/${req.params.id}/recommendations`)
 });
 
 
 app.listen(port, () => {
-  console.log(`server running at: http://${host}:${port}`);
+  console.log(`server running at: http://${hostPhotos}:${port}`);
 });
