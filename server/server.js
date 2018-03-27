489a72cfe21bd5118a3ae0c69e75203e71805d2f
zaid <zaidhusseini@gmail.com>
2018-03-26 17:34:15 -0700

    Set up server side rendering config

--- server/server.js
+++ server/server.js
@@ -4,23 +4,56 @@ const path = require("path");
 const bodyParser = require("body-parser");
 require("dotenv").load();
 
-//const morgan = require('morgan');
+const morgan = require('morgan');
 
 // var router = require("./routes/routes.js");
 const app = express();
+const host = 'localhost'
 const port = process.env.PORT || 3000;
 const hostPhotos = process.env.HOST_PHOTOS || 'localhost';
 const hostOverview = process.env.HOST_OVERVIEW || 'localhost';
 const hostSidebar = process.env.HOST_SIDEBAR || 'localhost';
 const hostRecommendations = process.env.HOST_RECOMMENDATIONS || 'localhost';
 
-//app.use(morgan('tiny'));
+app.use(morgan('tiny'));
 
-app.use('/restaurants/:id', express.static('public'));
+app.use('/restaurants/:id', express.static('public'));  //Capture static files (index.html, CSS, etc)
+
+const clientBundles = './public/services';
+const serverBundles = './templates/services';
+const serviceConfig = require('../service-config.json');
+const services = require('../loader.js')(clientBundles, serverBundles, serviceConfig);
+
+const React = require('react');
+const ReactDom = require('react-dom/server');
+const Layout = require('../templates/layout');
+const App = require('../templates/app');
+const Scripts = require('../templates/scripts');
+
+// see: https://medium.com/styled-components/the-simple-guide-to-server-side-rendering-react-with-styled-components-d31c6b2b8fbf
+const renderComponents = (components, props = {}) => {
+  return Object.keys(components).map(item => {
+    let component = React.createElement(components[item], props);
+    return ReactDom.renderToString(component);
+  });
+}
+
+
+app.get('/restaurants/:id', function(req, res){
+  let components = renderComponents(services);
+  res.end(Layout(
+    'Pen Apple PineApple Pen',
+    App(...components),
+    Scripts(Object.keys(services))
+  ));
+});
+
+
 
 app.get('/', (req, res) => {
   res.redirect(`http://${hostPhotos}:${port}/restaurants/${Math.floor(Math.random()*10000000)}`);
 });
+
 
 app.get('/api/restaurants/:id/gallery', (req, res) => {
   res.redirect(`http://${hostPhotos}:3002/api/restaurants/${req.params.id}/gallery`)
@@ -40,5 +73,7 @@ app.get('/api/restaurants/:id/recommendations', (req, res) => {
 
 
 app.listen(port, () => {
-  console.log(`server running at: http://${hostPhotos}:${port}`);
+  console.log(`server running at: http://${host}:${port}`);
 });
+
+
