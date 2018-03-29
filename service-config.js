85ce47648a3fcda85e5977761d6ba4216889c6a3
zaid <zaidhusseini@gmail.com>
2018-03-28 20:07:13 -0700

    Uncomment Sidebar bundle

--- service-config.js
+++ service-config.js
@@ -4,5 +4,5 @@ module.exports = {
    "Photos": `http://${process.env.HOST_PHOTOS}:${process.env.PORT_PHOTOS}/bundle`,
    "Recommendations": `http://${process.env.HOST_RECOMMENDATIONS}:${process.env.PORT_RECOMMENDATIONS}/bundle`,
    "Overview": `http://${process.env.HOST_OVERVIEW}:${process.env.PORT_OVERVIEW}/bundle`,
-  // "Sidebar": `http://${process.env.HOST_SIDEBAR}:${process.env.PORT_SIDEBAR}/bundle`,
+   "Sidebar": `http://${process.env.HOST_SIDEBAR}:${process.env.PORT_SIDEBAR}/bundle`,
 }
